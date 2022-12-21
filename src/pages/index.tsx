3fdba5f4a233d55bbfa206d15b9e2e098f99920c
Antonio Carlos <antoniotx.dev@gmail.com>
2022-12-21 00:45:48 -0300

    Altera estrutura de pastas

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import styles from '../styles/Form.module.css'
+import { NextPage } from 'next'
+
+const Form: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Form</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        Form works!!
+      </main>
+    </>
+  )
+}
+
+export default Form
