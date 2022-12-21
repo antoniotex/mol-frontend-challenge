998b71cb423540a4946aab73c884b14044e6ea95
Antonio Carlos <antoniotx.dev@gmail.com>
2022-12-21 01:20:29 -0300

    Refatora página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Form.module.css'
 import { NextPage } from 'next'
+import InputGroupTitle from '../components/InputGroupTitle'
 
 const Form: NextPage = () => {
   return (
@@ -10,8 +11,9 @@ const Form: NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        Form works!!
+      <main className={styles.main}>
+        <InputGroupTitle title='Novo solicitante' />
+        <InputGroupTitle title='Adicione dados de contato' />
       </main>
     </>
   )
